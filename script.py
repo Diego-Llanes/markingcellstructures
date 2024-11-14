9b9aacd321931c2750d0278f5ed75ffa7f7b5a98
Diego Llanes <snowythedog13@gmail.com>
2024-11-13 21:12:03 -0800

    Refactor

--- script.py
+++ script.py
@@ -2,6 +2,8 @@ import tifffile
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+# from sklearn.cluster import DBSCAN
+
 from pathlib import Path
 from typing import List, Tuple, Dict
 
@@ -18,6 +20,38 @@ notes:
  - data is shaped as (z, c, y, x)
 """
 
+def threshold_image(
+        img: np.ndarray,
+        z_slices: Sharpness,
+        threshold_ps: Tuple[float] = (0.81, 0.81, 0.81),
+        channels: List[str] = ["Cilia", "Golgi", "Cilia Base"],
+) -> cv2.threshold:
+    threses = []
+    for i, channel in enumerate(channels):
+        ret, thresh = cv2.threshold(
+            img[z_slices[i].z, i],
+            threshold_ps[i] * img[z_slices[i].z, i].max(),
+            img[z_slices[i].z, i].max(),
+            cv2.THRESH_BINARY
+        )
+        threses.append(thresh)
+    return np.stack(threses, axis=0)
+
+
+def find_contours(
+        thresh: np.ndarray,
+) -> None:
+    contours = []
+    for i in range(thresh.shape[0]):
+        contours.append(
+            cv2.findContours(
+                cv2.convertScaleAbs(thresh[i]),
+                cv2.RETR_EXTERNAL,
+                cv2.CHAIN_APPROX_SIMPLE,
+            )[0]
+        )
+    return contours
+
 
 def view_sample(
         img: np.ndarray,
@@ -26,40 +60,25 @@ def view_sample(
 ) -> None:
 
     # img has 3 channels, plot all
-    fig, axs = plt.subplots(3, 3, figsize=(15, 8))
+    fig, axs = plt.subplots(3, 3, figsize=(8, 8))
     channels = [
         "Cilia",
         "Golgi",
         "Cilia Base",
     ]
 
+    thresh = threshold_image(img, z_slices, threshold_ps)
+    contours = find_contours(thresh)
     for i, channel in enumerate(channels):
         axs[0][i].imshow(img[z_slices[i].z, i], cmap='gray')
         axs[0][i].set_title(f'{channel} Channel')
 
-        # apply binary thresholding
-        ret, thresh = cv2.threshold(
-            img[z_slices[i].z, i],
-            threshold_ps[i] * img[z_slices[i].z, i].max(),
-            img[z_slices[i].z, i].max(),
-            cv2.THRESH_BINARY
-        )
-
-        axs[1][i].imshow(thresh, cmap='gray')
+        axs[1][i].imshow(thresh[i], cmap='gray')
         axs[1][i].set_title(f'{channel} Channel Thresholded to {threshold_ps[i] * 100}%')
 
-        contours, _ = cv2.findContours(
-            cv2.convertScaleAbs(thresh),
-            cv2.RETR_EXTERNAL,
-            cv2.CHAIN_APPROX_SIMPLE,
-        )
-        all_points = np.concatenate(contours)
-        hull = cv2.convexHull(all_points)
-        cv2.drawContours(img[z_slices[i].z, i], [hull], 0, (0, 255, 0), 2)
-        plt.imshow(img[z_slices[i].z, i], cmap='gray')
-
-
-
+        cv2.drawContours(img[z_slices[i].z, i], contours[i], -1, (0, 255, 0), 2)
+        axs[2][i].imshow(img[z_slices[i].z, i], cmap='gray')
+        axs[2][i].set_title(f'{channel} Channel Contours')
 
     plt.show()
 
@@ -88,11 +107,18 @@ def find_best_zslices(
         return {c: max(sharpness, key=lambda x: x.sharpness) for c, sharpness in channels.items()}
     return channels
 
-def threshold_image(
+
+def find_clusters(
         img: np.ndarray,
-        threshold: int,
-) -> np.ndarray:
-    return np.where(img > threshold, 255, 0).astype(np.uint8)
+        z_slices: Sharpness,
+) -> None:
+    dbscan = DBSCAN(eps=0.5, min_samples=5)
+    fit = dbscan.fit(img[z_slices[0].z, 0])
+    labels = fit.labels_
+
+    n_clusters = len(set(labels)) - (1 if -1 in labels else 0)
+    import ipdb; ipdb.set_trace()
+
 
 def main():
     files = list(DATA_DIR.glob('*.tif'))
